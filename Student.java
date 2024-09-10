77efe6bd43cc165a7ee809b581423236e51783ab
Jocelynnn11 <110930776+Jocelynnn11@users.noreply.github.com>
2024-09-10 12:10:19 -0700

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,19 @@
+public class Student {
+    public static int numOfStudents = 0;
+
+    // Give our Student class a few fields
+    public Student() {
+        numOfStudents++;
+    }
+
+    // Give our Student class a few fields
+    String firstName;
+    String lastName;
+    String major;
+    double gpa;
+    int age;
+    boolean onProbation;
+
+
+
+}
